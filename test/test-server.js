f301ce9963db5d86a97581ea33f39c8660982ce3
sfespinosa <scottjames.espinosa@gmail.com>
2021-07-28 17:37:45 +0000

    updated tests

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1 class='text-success fs-1 text'>Welcome to Team Natural's application</h1>"));
         return done();
       });
 
